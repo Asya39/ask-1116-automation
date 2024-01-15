018e2c456800603f8e46956bbf0f9ca5426c5276
Asya39 <89507082+Asya39@users.noreply.github.com>
2024-01-15 13:17:33 -0800

    Update TestContext.java

--- ask-init-local11/src/test/java/support/TestContext.java
+++ ask-init-local11/src/test/java/support/TestContext.java
@@ -1,6 +1,6 @@
 // Created by Viacheslav (Slava) Skryabin 04/01/2011
 package support;
-
+ 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
@@ -17,28 +17,28 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
+ 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
+ 
 public class TestContext {
-
+ 
     private static WebDriver driver;
-
+ 
     public static WebDriver getDriver() {
         return driver;
     }
-
+ 
     public static void initialize() {
         initialize("chrome", "local", false, false);
     }
-
+ 
     public static void teardown() {
         driver.quit();
     }
-
+ 
     public static void initialize(String browser, String testEnv, boolean isHeadless, boolean isJenkins) {
         Dimension size = new Dimension(1920, 1080);
         Point position = new Point(0, 0);
